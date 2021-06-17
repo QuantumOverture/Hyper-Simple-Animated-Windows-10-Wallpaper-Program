4ebebeffdd0f73a2ca9ab2975dd3ff8c830056fa
ismaildude <38566647+ismaildude@users.noreply.github.com>
2021-06-16 17:19:44 -0700

    Add files via upload

---
+++ Wallpaper.cpp
@@ -0,0 +1,26 @@
+#include <windows.h>
+#include <string>
+
+
+
+
+
+int main() {
+	
+	int counter = 0;
+	while(true){
+	if(counter == 10){
+		counter = 0;
+	}
+	
+    std::string s = "C:\\Users\\ismai\\Downloads\\waves\\frame_"+std::to_string(counter)+"_delay-0.1s.gif";
+	
+    SystemParametersInfoA(SPI_SETDESKWALLPAPER, 0, (PVOID*)s.c_str(), SPIF_SENDCHANGE);
+	
+	Sleep(100);
+	
+	counter++;
+	
+	}
+    return 0;
+}
